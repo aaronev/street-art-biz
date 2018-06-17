d2461452f95c7fb19e570ed41bce062d5877e9eb
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-17 12:09:28 -0700

    starts work on each section of the main tag

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 console.log('JS WORKS!')
 
 var dom = {
-  "home": document.getElementById("home"),
+  home: document.getElementById("home"),
   about: document.getElementById("about"),
-
+  art: document.getElementById("art")
 }
 
 dom.home.addEventListener("click", () => {
   if (dom.about.style.display === "none") {
-    console.log('hello')
     dom.about.style.display = "block"
   } else {
     dom.about.style.display = "none"
