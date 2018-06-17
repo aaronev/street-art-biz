eda06d59a8e5361558bedabe8a3ee05628e76523
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-17 12:35:23 -0700

    adds upload or choose art section to have display none or block

--- js/index.js
+++ js/index.js
@@ -2,14 +2,24 @@ console.log('JS WORKS!')
 
 var dom = {
   home: document.getElementById("home"),
+  navArt: document.getElementById("nav-art"),
   about: document.getElementById("about"),
   art: document.getElementById("art")
 }
 
 dom.home.addEventListener("click", () => {
-  if (dom.about.style.display === "none") {
-    dom.about.style.display = "block"
-  } else {
+  if (dom.about.style.display === "block") {
     dom.about.style.display = "none"
+  } else {
+    dom.about.style.display = "block"
   }
 })
+
+dom.navArt.addEventListener("click", () => {
+  if (dom.art.style.display === "block") {
+    dom.art.style.display = "none"
+  } else {
+    dom.art.style.display = "block"
+  }
+})
+
